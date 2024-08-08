cab6b18a5489fb68ff6e4c56b0cc3c62a70c92d4
yuinakorn <yuinakorn@hotmail.com>
2024-08-08 14:43:35 +0700

    update2

---
+++ db.mjs
@@ -0,0 +1,13 @@
+import mysql from 'mysql';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
+});
+
